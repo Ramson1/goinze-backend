92d2eb5056b409aeb2732ee2707233f47cc37a33
kachi <blackboxinfo01@gmail.com>
2026-09-01 00:12:51 +0100

    fix: import CommunicationModule in AlumniModule

--- src/alumni/alumni.module.ts
+++ src/alumni/alumni.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AlumniController } from './alumni.controller';
 import { AlumniService } from './alumni.service';
+import { CommunicationModule } from '../communication/communication.module';
 
 @Module({
+  imports: [CommunicationModule],
   controllers: [AlumniController],
   providers: [AlumniService],
 })
